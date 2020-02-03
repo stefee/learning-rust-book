a967a588346d7d116dea9f2778bd117bcceb128e
Stef <stef.jones@condenast.com>
2020-02-03 00:43:38 +0000

    Add enums

---
+++ enums/src/main.rs
@@ -0,0 +1,101 @@
+fn main() {
+    l61();
+    l61b();
+    l62();
+    l63();
+}
+
+fn l61() {
+    #[derive(Debug)]
+    enum IpAddrKind {
+        V4,
+        V6
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("The home IP address is {} kind {:?}.", home.address, home.kind);
+    println!("The loopback IP address is {} kind {:?}.", loopback.address, loopback.kind);
+}
+
+fn l61b() {
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!("The home IP address is {:?}.", home);
+    println!("The loopback IP address is {:?}.", loopback);
+}
+
+fn l62() {
+    #[derive(Debug)]
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            println!("The message is {:?}.", self);
+        }
+    }
+
+    let q = Message::Quit;
+    let m = Message::Move { x: 20, y: 30 };
+    let w = Message::Write(String::from("hello"));
+    let c = Message::ChangeColor(1, 2, 3);
+
+    q.call();
+    m.call();
+    w.call();
+    c.call();
+}
+
+fn l63() {
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    let penny = value_in_cents(Coin::Penny);
+    let nickel = value_in_cents(Coin::Nickel);
+    let dime = value_in_cents(Coin::Dime);
+    let quarter = value_in_cents(Coin::Quarter);
+
+    println!("Penny is {}.", penny);
+    println!("Nickel is {}.", nickel);
+    println!("Dime is {}.", dime);
+    println!("Quarter is {}.", quarter);
+}
